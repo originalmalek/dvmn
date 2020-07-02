e4e11b19682d5418ea64546d27bb0f72057eab13
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-07-02 22:23:06 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,14 +1,19 @@
 import requests
 
-cities = ['лондон', 'шереметьево', 'череповец']
 
-payload = {'nTq': '', 'lang': 'ru'}
+def main()
+    cities = ['лондон', 'шереметьево', 'череповец']
+    
+    payload = {'nTq': '', 'lang': 'ru'}
 
-for city in cities:
-    url = 'http://wttr.in/' + city
-    r = requests.get(url, params=payload)
-    print(r.url)
-    if r.ok:
-        print(r.text)
-    else:
-        r.raise_for_status()
+    for city in cities:
+        url = 'http://wttr.in/' + city
+        r = requests.get(url, params=payload)
+        if r.ok:
+            print(r.text)
+        else:
+            print('Response error' + r.status_code)
+
+
+if __name__ == '__main__':
+    main()
